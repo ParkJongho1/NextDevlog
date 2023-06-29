10eebf10951da82f9bc5b0edef28b5cf4e62aa62
ParkJongho1 <godhojong1@gmail.com>
2023-06-29 16:58:07 +0900

    Devlog first commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
       <nav className="sticky top-0 z-10 p-4 bg-slate-600 drop-shadow-xl">
          <div className="flex flex-col justify-between mx-auto prose prose-xl sm:flex-row">
             <Link href="/" className="no-underline text-white/90 hover:text-white">
-               Jongho`Devlog
+               Jongho`Devlog.
             </Link>
          </div>
       </nav>
