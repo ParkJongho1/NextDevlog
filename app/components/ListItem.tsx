ffa5dfe82c306e081726fbd2979288fe6c1804cb
Parkjongho1 <godhojong1@gmail.com>
2023-07-05 22:30:21 +0900

    bloglist

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+import getFormattedDate from "@/lib/getFormattedDate";
+
 type Props = {
   post: BlogPost;
 };
 
 export default function ListItem({ post }: Props) {
   const { id, title, date } = post;
+  const formatterDate = getFormattedDate(date);
 
   return <div>ListItem</div>;
 }
