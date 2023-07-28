c2f69fb5e69a51cb2152d4d87af0d6258699d82b
ParkJongho1 <godhojong1@gmail.com>
2023-07-28 11:24:16 +0900

    contentlayer 구조로 변경

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+   return (
+      <div className="max-w-3xl pl-4 pr-4 mx-auto mt-5 lg:max-w-6xl">
+         <h1 style={{ color: "white" }}>about</h1>
+      </div>
+   );
+}
